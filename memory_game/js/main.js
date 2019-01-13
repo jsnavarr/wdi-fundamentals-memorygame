4cd5969a64590e1b7b30cff30a54ff73406fbd7c
j salvador navarro <salvador.navarro@gmail.com>
2019-01-12 20:07:22 -0600

    added event handlers in javascript for user interaction

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,27 +23,71 @@ var cards = [
 	},
 ];
 
-
 var cardsInPlay = [];
+var attempts = 0;
+var matches = 0;
+
+var setScore = function(){
+	var newAttempts = document.getElementById('attempts');
+	newAttempts.innerHTML = attempts + " ";
+	var newMatches = document.getElementById('matches');
+	newMatches.innerHTML = matches + "";
+}
+
+var cleanBoard = function(){
+	imgs = document.getElementsByTagName('img');
+	for (var i=0; i<imgs.length; i++){
+		imgs[i].setAttribute('src', 'images/back.png');
+	}
+	cardsInPlay = [];
+};
+
+var resetGame = function(){
+	cleanBoard();
+	attempts = 0;
+	matches = 0;
+	setScore();
+};
 
 var checkForMatch = function() {
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]){
 			alert("You found a match!");
+			matches++;
 		} else {
 			alert("Sorry, try again!");
+			
 		}
+		attempts++;
+		setScore();
 	}
-}
+};
 
-var flipCard = function(cardId){
-	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].suit);
-	console.log(cards[cardId].cardImage);
-	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch();
-}
+var flipCard = function(){
+	if(cardsInPlay.length < 2){
+		var cardId = this.getAttribute('data-id');
+		this.setAttribute('src', cards[cardId].cardImage);
+		console.log("User flipped " + cards[cardId].rank);
+		cardsInPlay.push(cards[cardId].rank);
+		checkForMatch();
+	}
+};
 
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+	var cardElement;
+	for (var i = 0; i < cards.length; i++) {
+    	cardElement = document.createElement('img');
+    	cardElement.setAttribute('src', 'images/back.png');
+    	cardElement.setAttribute('data-id', i);
+    	cardElement.addEventListener('click', flipCard);
+    	document.getElementById('game-board').appendChild(cardElement);
+	}
+	resetButton = document.getElementById('reset-button');
+	resetButton.addEventListener('click', resetGame);
+	PlayAgainButton = document.getElementById('play-again-button');
+	PlayAgainButton.addEventListener('click', cleanBoard);
+};
+
+createBoard();
+
 
